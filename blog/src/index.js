44e911bae426f80297bcba7941fdcaa03929505e
levanthanhlong <thanhtdbn132001@gmail.com>
2023-07-17 19:17:06 +0700

    s

--- blog/src/index.js
+++ blog/src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
+const route = require('./routes')
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -11,20 +12,23 @@ app.use(morgan('combined'));
 // file static
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json({
+   deprecated: true
+}));
+
 // template handlebars
 app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine','hbs')
 app.set("views", path.join(__dirname, "resources/views"));
-console.log("path: ",path.join(__dirname, "resources","views"))
 
 
-app.get('/home', (req, res) => {
-  res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// route init 
+route(app);
 
+// add port
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 })  
