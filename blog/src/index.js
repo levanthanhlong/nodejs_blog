0184cce8351a953a3c1271c9c701036d742ae24d
levanthanhlong <thanhtdbn132001@gmail.com>
2023-07-18 11:19:11 +0700

    hihi

--- blog/src/index.js
+++ blog/src/index.js
@@ -25,7 +25,7 @@ app.use(
 
 // template handlebars
      app.engine('.hbs', handlebars({ extname: '.hbs' }));
-           app.set('view engine', 'hbs');
+        app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // route init
