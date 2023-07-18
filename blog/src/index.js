e54f816702b2bdae00313485811804f218d8efc5
levanthanhlong <thanhtdbn132001@gmail.com>
2023-07-18 11:29:56 +0700

    hihi

--- blog/src/index.js
+++ blog/src/index.js
@@ -23,8 +23,8 @@ app.use(
     }),
 );
 
-// template handlebars 
-        app.engine('.hbs', handlebars({ extname: '.hbs' }));
+// template handlebars
+app.engine('.hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
