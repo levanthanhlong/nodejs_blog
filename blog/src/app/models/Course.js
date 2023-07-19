89884d8abd3129d7fff8100747c68684ce423a3b
levanthanhlong <thanhtdbn132001@gmail.com>
2023-07-20 01:40:18 +0700

    qua met

--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
 
 const Course = new Schema({
-    name: {type: String, maxLength: 255, require: true},
+    name: {type: String, maxLength: 255, required: true},
     description: {type: String, maxLength: 1000},
     image: {type: String}, 
-    videoid: {type: String, require: true}, 
+    videoid: {type: String, required: true},
+    slug: { type: String, slug: 'name'},
   },
   {
     timestamps: true,
